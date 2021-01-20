d280203bb5a7c916a4acb9545fb4345c29051df1
dechabhol kotheeranurak <first_dejabhol@hotmail.com>
2021-01-20 18:08:22 +0700

    fixing things

--- cmd/publisherd/publisherd.go
+++ cmd/publisherd/publisherd.go
@@ -1,50 +1,12 @@
 package main
 
 import (
-	"html/template"
-	"log"
-	"net/http"
-	"os"
+	"fmt"
+
+	"github.com/wisatong/potato/publisherd"
 )
 
-//Student something
-type Student struct {
-	Name      string
-	College   string
-	StudentID int
-}
-
-func getTemplate() *template.Template {
-	dir, _ := os.Getwd()
-	templates := template.Must(template.ParseFiles(dir+"/templates/test.html", dir+"/templates/index.html"))
-	return templates
-}
-
-//Handler something
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	student := Student{
-		Name:      "John Doe",
-		College:   "Kasetsart",
-		StudentID: 601054000,
-	}
-	err := getTemplate().ExecuteTemplate(w, "test.html", student)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	err := getTemplate().ExecuteTemplate(w, "index.html", "")
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 func main() {
-	// Start publisherd
-	http.HandleFunc("/", testHandler)
-	http.HandleFunc("/index", indexHandler)
-	http.ListenAndServe(":8000", nil)
 
+	fmt.Println(publisherd.GetStaticFile("index.html").Name())
 }
--- publisherd/publisherd.go
+++ publisherd/publisherd.go
@@ -1,1 +1,21 @@
 package publisherd
+
+import (
+	"os"
+)
+
+func GetStaticFile(filename string) (fp *os.File) {
+	dir := os.Getenv("ASSET_DIR")
+	fp, err := os.Open(dir + "/" + filename)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := fp.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	return fp
+}
