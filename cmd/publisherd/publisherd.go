ed2e1cbb7363ce845f788655eb7bfbb27b7ef7b3
wisatong <wisa.pow@gmail.com>
2021-01-21 01:59:38 +0700

    added start daemon method for publisherd

--- cmd/publisherd/publisherd.go
+++ cmd/publisherd/publisherd.go
@@ -1,25 +1,12 @@
 package main
 
 import (
-	"log"
-	"net"
-	"net/rpc"
 	"os"
 
 	"github.com/wisatong/potato/publisherd"
 )
 
 func main() {
-	address, err := net.ResolveTCPAddr("tcp", "0.0.0.0:12345")
-	if err != nil {
-		log.Fatal(err)
-	}
-	inbound, err := net.ListenTCP("tcp", address)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	d := publisherd.Publisherd{StaticDir: os.Getenv("ASSET_DIR")}
-	rpc.Register(&d)
-	rpc.Accept(inbound)
+	d := publisherd.Publisherd{StaticDir: os.Getenv("STATIC_DIR")}
+	d.Start(7525)
 }
