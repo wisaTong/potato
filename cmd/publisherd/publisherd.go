f9463161c5d45216358eeccd9988f7e321c15550
dechabhol kotheeranurak <first_dejabhol@hotmail.com>
2021-01-20 14:37:50 +0700

    handle something

--- cmd/publisherd/publisherd.go
+++ cmd/publisherd/publisherd.go
@@ -1,10 +1,38 @@
 package main
 
-import "fmt"
-import pub "github.com/wisaTong/publisherd"
- 
+import (
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+//Student something
+type Student struct {
+	Name      string
+	College   string
+	StudentID int
+}
+
+//Handler something
+func handler(w http.ResponseWriter, r *http.Request) {
+	student := Student{
+		Name:      "John Doe",
+		College:   "Kasetsart",
+		StudentID: 601054000,
+	}
+	dir, _ := os.Getwd()
+	t, _ := template.ParseFiles(dir + "/templates/test.html")
+	err := t.Execute(w, student)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
 
 func main() {
 	// Start publisherd
-	fmt.Println("Hi")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8000", nil)
+
 }
--- publisherd/publisherd.go
+++ publisherd/publisherd.go
@@ -1,8 +1,1 @@
 package publisherd
-
-import "fmt"
-
-// This func must be Exported, Capitalized, and comment added.
-func Demo() {
-    fmt.Println("HI")
-}
