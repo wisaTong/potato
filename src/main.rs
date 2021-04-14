44a5b2596c742d682158b06969375a7391ec24e7
wisatong <wisa.pow@gmail.com>
2021-04-14 14:10:20 +0700

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use libc;
 use nix::unistd;
 use potato::clone;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use libc;
 
 struct PotatoResponse {
     status_code: String,
@@ -21,8 +21,7 @@ impl PotatoResponse {
 }
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
+    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
@@ -50,32 +49,24 @@ fn get_hostname() -> PotatoResponse {
 
 fn set_and_get_hostname() -> PotatoResponse {
     unistd::sethostname("boba").unwrap();
-    let status_code = "200 Ok".to_string();
-    let header = "".to_string();
-    let body = get_hostname_as_string();
-    let response = PotatoResponse {
-        status_code,
-        header,
-        body,
-    };
-    println!("gello");
-    response
+    PotatoResponse {
+        status_code: "200 OK".to_string(),
+        header: "".to_string(),
+        body: get_hostname_as_string(),
+    }
 }
 
 fn write_response<F>(logic: F, mut stream: &TcpStream) -> isize
 where
     F: FnOnce() -> PotatoResponse,
 {
-    let response = logic();
-    stream
-        .write(response.to_http_response().as_bytes())
-        .unwrap();
+    let response = logic().to_http_response();
+    stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
     0
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    // --snip--
     let get = b"GET / HTTP/1.1\r\n";
     let get_ns = b"GET /ns HTTP/1.1\r\n";
 
@@ -87,18 +78,18 @@ fn handle_connection(mut stream: TcpStream) {
 
     if buffer.starts_with(get) {
         let cb = || write_response(get_hostname, &stream);
-        clone::clone_proc_newns(
-            cb,
-            stack,
-            libc::CLONE_NEWUTS | libc::CLONE_NEWNET,
-        );
+        clone::clone_proc_newns(cb, stack, libc::CLONE_NEWUTS | libc::CLONE_NEWNET);
     }
     if buffer.starts_with(get_ns) {
         let cb = || write_response(set_and_get_hostname, &stream);
         clone::clone_proc_newns(
             cb,
             stack,
-            libc::CLONE_NEWUTS | libc::CLONE_NEWNET | libc::CLONE_VM | libc::CLONE_THREAD | libc::CLONE_SIGHAND,
+            libc::CLONE_NEWUTS
+                | libc::CLONE_NEWNET
+                | libc::CLONE_VM
+                | libc::CLONE_THREAD
+                | libc::CLONE_SIGHAND,
         );
     }
 }
