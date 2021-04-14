7e8c210ee63dea6fc490f9b2afdaa15338e173bc
wisatong <wisa.pow@gmail.com>
2021-04-14 16:16:16 +0700

    added fs_prep

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use libc;
 use nix::unistd;
 use potato::clone;
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -21,6 +22,11 @@ impl PotatoResponse {
 }
 
 fn main() {
+    // create potato dir for each request if not exist
+    let uid = unistd::getuid();
+    let runtime_dir = format!("/var/run/user/{}/potato", uid);
+    fs::create_dir_all(runtime_dir).expect("Faild to create runtime dir");
+
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -35,35 +41,51 @@ fn get_hostname_as_string() -> String {
     hostname.to_string()
 }
 
-fn get_hostname() -> PotatoResponse {
+fn get_hostname(_: &TcpStream) -> PotatoResponse {
     let status_code = "200 Ok".to_string();
-    let header = "".to_string();
     let body = get_hostname_as_string();
-    let response = PotatoResponse {
+    let header = format!("Content-Length: {}", body.len());
+    PotatoResponse {
         status_code,
         header,
         body,
-    };
-    response
-}
-
-fn set_and_get_hostname() -> PotatoResponse {
-    unistd::sethostname("boba").unwrap();
-    PotatoResponse {
-        status_code: "200 OK".to_string(),
-        header: "".to_string(),
-        body: get_hostname_as_string(),
     }
 }
 
-fn write_response<F>(logic: F, mut stream: &TcpStream) -> isize
+fn set_and_get_hostname(_: &TcpStream) -> PotatoResponse {
+    unistd::sethostname("boba").unwrap();
+    let body = get_hostname_as_string();
+
+    PotatoResponse {
+        status_code: "200 OK".to_string(),
+        header: format!("Content-Length: {}", body.len()),
+        body,
+    }
+}
+
+fn isolate_request<T, F>(mut stream: TcpStream, task: T, fs_prep: F) -> isize
 where
-    F: FnOnce() -> PotatoResponse,
+    T: FnOnce(&TcpStream) -> PotatoResponse,
+    F: FnOnce(), // TODO return type?
 {
-    let response = logic().to_http_response();
+    fs_prep();
+    let response = task(&stream).to_http_response();
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
     0
+}
+
+fn fs_prep() {
+    let uid = unistd::getuid();
+    let pid = unistd::getpid();
+    println!("fs prep pid: {}", pid);
+    let rootfs = format!("/var/run/user/{}/potato/{}", uid, pid);
+    println!("rootfs: {}", rootfs);
+
+    // TODO handle error
+    fs::create_dir_all(rootfs.as_str()).unwrap();
+    unistd::chroot(rootfs.as_str()).unwrap();
+    unistd::chdir(".").unwrap();
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -77,19 +99,16 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     if buffer.starts_with(get) {
-        let cb = || write_response(get_hostname, &stream);
-        clone::clone_proc_newns(cb, stack, libc::CLONE_NEWUTS | libc::CLONE_NEWNET);
-    }
-    if buffer.starts_with(get_ns) {
-        let cb = || write_response(set_and_get_hostname, &stream);
-        clone::clone_proc_newns(
-            cb,
-            stack,
-            libc::CLONE_NEWUTS
-                | libc::CLONE_NEWNET
-                | libc::CLONE_VM
-                | libc::CLONE_THREAD
-                | libc::CLONE_SIGHAND,
-        );
+        let cb = || isolate_request(stream, get_hostname, fs_prep);
+        let flags = libc::CLONE_NEWUTS | libc::CLONE_NEWNET;
+        clone::clone_proc_newns(cb, stack, flags);
+    } else if buffer.starts_with(get_ns) {
+        let cb = || isolate_request(stream, set_and_get_hostname, fs_prep);
+        let flags = libc::CLONE_NEWUTS
+            | libc::CLONE_NEWNET
+            | libc::CLONE_VM
+            | libc::CLONE_THREAD
+            | libc::CLONE_SIGHAND;
+        clone::clone_proc_newns(cb, stack, flags);
     }
 }
