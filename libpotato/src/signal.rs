172fb71b3c07e619e658273e3d09deaf9e360466
wisatong <wisa.pow@gmail.com>
2021-04-27 02:36:19 +0700

    sigprocmask will never failed return void instead of result

--- libpotato/src/signal.rs
+++ libpotato/src/signal.rs
@@ -7,10 +7,10 @@ use nix::sys::wait;
 /// Change the signal mask of the calling thread through `sigprocmask(2)`.
 /// The argument `how` is always set to `SIG_BLOCK`.
 /// The set of blocked signals is the union of the current set and the set argument.
-pub fn block(set: &[signal::Signal]) -> Result<(), nix::Error> {
+pub fn block(set: &[signal::Signal]) {
     let ref sigset = sigset_from_slice(set);
     let how = signal::SigmaskHow::SIG_BLOCK;
-    signal::sigprocmask(how, Some(sigset), None)
+    signal::sigprocmask(how, Some(sigset), None).unwrap();
 }
 
 /// Unblock signals for calling thread
@@ -19,10 +19,10 @@ pub fn block(set: &[signal::Signal]) -> Result<(), nix::Error> {
 /// The argument `how` is always set to `SIG_UNBLOCK`.
 /// The signals in set are removed from the current set of blocked signals.
 /// It is permissible to attempt to unblock a signal which is not blocked.
-pub fn unblock(set: &[signal::Signal]) -> Result<(), nix::Error> {
+pub fn unblock(set: &[signal::Signal]) {
     let ref sigset = sigset_from_slice(set);
     let how = signal::SigmaskHow::SIG_UNBLOCK;
-    signal::sigprocmask(how, Some(sigset), None)
+    signal::sigprocmask(how, Some(sigset), None).unwrap();
 }
 
 /// Replace the set of blocked signals with the new set for calling thread
@@ -30,10 +30,10 @@ pub fn unblock(set: &[signal::Signal]) -> Result<(), nix::Error> {
 /// Change the signal mask of the calling thread through `sigprocmask(2)`.
 /// The argument `how` is always set to `SIG_SETMASK`.
 /// The set of blocked signals is set to the argument set.
-pub fn setmask(set: &[signal::Signal]) -> Result<(), nix::Error> {
+pub fn setmask(set: &[signal::Signal]) {
     let ref sigset = sigset_from_slice(set);
     let how = signal::SigmaskHow::SIG_SETMASK;
-    signal::sigprocmask(how, Some(sigset), None)
+    signal::sigprocmask(how, Some(sigset), None).unwrap();
 }
 
 fn sigset_from_slice(signals: &[signal::Signal]) -> SigSet {
